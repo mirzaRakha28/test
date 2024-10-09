16d5c2bfefd4a2cc985a6659f5a3ab51ab80f3f7
mirzarakha28 <mirzarakhadwipradana28@gmail.com>
2024-10-09 16:45:49 +0700

    test2

--- main.go
+++ main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // Struct for JSON response
 type Response struct {
-	Message string `json:"message"`
+	Message      string `json:"message"`
+	RandomNumber int    `json:"random_number"`
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
+	// Set content type to JSON
 	w.Header().Set("Content-Type", "application/json")
-	response := Response{Message: "Hello, World!"}
+
+	// Seed random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate random number between 1 and 100
+	randomNumber := rand.Intn(100) + 1
+
+	// Create response with message and random number
+	response := Response{
+		Message:      "Hello, World!",
+		RandomNumber: randomNumber,
+	}
+
+	// Send JSON response
 	json.NewEncoder(w).Encode(response)
 }
 
